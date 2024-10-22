ad1359b22c3a76a5cc9d5a6bb79902b478d546ad
ourdreanm <94443026+ourdreanm@users.noreply.github.com>
2024-10-22 23:56:57 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -1,13 +1,11 @@
 const pageConfig = {
   // Title for your status page
-  title: "AM科技's Status Page",
+  title: "潇恒's Status Page",
   // Links shown at the header of your status page, could set `highlight` to `true`
-  links: [
-    { link: 'https://am.809098.xyz', label: '个人博客', highlight: true },
-    { link: 'https://youtube.com/@AM_CLUB', label: 'AM科技' },
-    { link: 'https://github.com/amclubs', label: 'GitHub' },
-    { link: 'https://809098.xyz', label: 'Blog'},
-  ],
+  llinks: [
+  { link: 'https://z.962456.xyz', label: '影视站' },
+  { link: 'https://xhy23-qinglong-01.hf.space', label: '青龙', highlight: true },
+],
 }
 
 const workerConfig = {
@@ -20,17 +18,17 @@ const workerConfig = {
     // Example HTTP Monitor
     {
       // `id` should be unique, history will be kept if the `id` remains constant
-      id: 'am.809098.xyz',
+      id: 'z.962456.xyz',
       // `name` is used at status page and callback message
-      name: '个人博客',
+      name: '影视站',
       // `method` should be a valid HTTP Method
       method: 'GET',
       // `target` is a valid URL
-      target: 'https://am.809098.xyz',
+      target: 'https://z.962456.xyz',
       // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
       tooltip: 'This is a tooltip for this monitor',
       // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
-      statusPageLink: 'https://am.809098.xyz',
+        // statusPageLink: 'https://am.809098.xyz',
       // [OPTIONAL] `expectedCodes` is an array of acceptable HTTP response codes, if not specified, default to 2xx
       // expectedCodes: [200],
       // [OPTIONAL] `timeout` in millisecond, if not specified, default to 10000
@@ -50,14 +48,14 @@ const workerConfig = {
     },
     // Example TCP Monitor
     {
-      id: '809098.xyz',
-      name: 'Blog',
+      id: 'xhy23-qinglong-01.hf.space',
+      name: '青龙',
       // `method` should be `TCP_PING` for tcp monitors
       method: 'GET',
       // `target` should be `host:port` for tcp monitors
-      target: 'https://809098.xyz',
+      target: 'https://xhy23-qinglong-01.hf.space',
       tooltip: 'My production server monitor',
-      statusPageLink: 'https://809098.xyz',
+      //statusPageLink: 'https://809098.xyz',
       timeout: 10000,
     },
   ],
